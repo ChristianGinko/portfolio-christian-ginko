87c3abc3c31655f68fb590c8820501d9bec209d8
ChristianGinko <christianginko@gmail.com>
2025-11-28 16:17:35 -0300

    Cambios

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -3,6 +3,11 @@ import christianGinko from './Christian Ginko.jpg';
 import pokeapi from './Pokeapi.jpg';
 import pokedex from './Pokedex.jpeg';
 import starwars from './StarWars.png';
+import email from './Email logo.png';
+import instagram from './Instagram logo.png';
+import github from './GitHub logo.png';
+import linkedin from './Linkedin logo.png';
+import x from './X logo.png';
 
 function Portfolio () {
     return (
@@ -103,19 +108,24 @@ function Portfolio () {
             </div>
             <div className="DatosContacto">
                 <li className="item">
+                    <img src={email} className="icon" alt="email" />
                     <span className="label">Email:</span>{" "}christianginko@gmail.com
                 </li>
                 <li className="item">
-                    <span className="label">Instagram:</span>{" "}<a href="https://www.instagram.com/christianginko/">christianginko</a>
+                    <img src={instagram} className="icon" alt="instagram" />
+                    <span className="label">Instagram:</span>{" "}<a className="LinkCustom" href="https://www.instagram.com/christianginko/">christianginko</a>
                 </li>
                 <li className="item">
-                    <span className="label">Twitter:</span>{" "}<a href="https://x.com/CGinko">CGinko</a>
+                    <img src={x} className="icon" alt="x" />
+                    <span className="label">Twitter:</span>{" "}<a className="LinkCustom" href="https://x.com/CGinko">CGinko</a>
                 </li>
                 <li className="item">
-                    <span className="label">Linkedin:</span>{" "}<a href="https://www.linkedin.com/in/christian-ariel-ginko/">Christian Ariel Ginko</a>
+                    <img src={linkedin} className="icon" alt="linkedin" />
+                    <span className="label">Linkedin:</span>{" "}<a className="LinkCustom" href="https://www.linkedin.com/in/christian-ariel-ginko/">Christian Ariel Ginko</a>
                 </li>
                 <li className="item">
-                    <span className="label">GitHub:</span>{" "}<a href="https://github.com/ChristianGinko">ChristianGinko</a>
+                    <img src={github} className="icon" alt="github" />
+                    <span className="label">GitHub:</span>{" "}<a className="LinkCustom" href="https://github.com/ChristianGinko">ChristianGinko</a>
                 </li>
             </div>
         </div>
