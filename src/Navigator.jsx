aae1fda27aef592927ffbf37aef24aed4983badf
ChristianGinko <christianginko@gmail.com>
2025-12-01 16:24:39 -0300

    Cambios

--- src/Navigator.jsx
+++ src/Navigator.jsx
@@ -1,9 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Portfolio from './Portfolio/Portfolio.js';
+import christianGinko from './Portfolio/Christian Ginko.jpg';
+import './Portfolio/Portfolio.css';
 
 function Navigator() {
     return (
         <BrowserRouter basename="/portfolio-christian-ginko">
+            <div className="Presentacion">
+                <div className="Horizontal">
+                    <img src={christianGinko} className="FotoPerfil" alt="christianGinko" />
+                    <div className="Vertical">
+                        <h1 className="Nombre">Christian Ginko</h1>
+                        <h2 className="Profesion">Software Developer</h2>
+                    </div>
+                </div>
+            </div>
             <Routes>
                 <Route path="/" element={<Portfolio />} />
             </Routes>
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,5 +1,4 @@
 import './Portfolio.css';
-import christianGinko from './Christian Ginko.jpg';
 import pokeapi from './Pokeapi.jpg';
 import pokedex from './Pokedex.jpeg';
 import starwars from './StarWars.png';
@@ -12,15 +11,6 @@ import x from './X logo.png';
 function Portfolio () {
     return (
         <div className="Portfolio">
-            <div className="Presentacion">
-                <div className="Horizontal">
-                    <img src={christianGinko} className="FotoPerfil" alt="christianGinko" />
-                    <div className="Vertical">
-                        <h1 className="Nombre">Christian Ginko</h1>
-                        <h2 className="Profesion">Software Developer</h2>
-                    </div>
-                </div>
-            </div>
             <div className="Cuerpo">
                 <h2 style={{fontFamily: 'Lucida Sans', fontStyle: 'italic', fontWeight: 'bold', textDecoration: 'underline'}}>Sobre mí</h2>
                 <p style={{fontFamily: 'Arial', fontStyle: 'oblique'}}>
