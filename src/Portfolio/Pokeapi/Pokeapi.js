bc91e8eeb2a60cb4e581fc1290ff812f90c6a310
ChristianGinko <christianginko@gmail.com>
2025-12-03 15:13:25 -0300

    Cambios

--- src/Portfolio/Pokeapi/Pokeapi.js
+++ src/Portfolio/Pokeapi/Pokeapi.js
@@ -1,8 +1,13 @@
 import './Pokeapi.css';
+import listaPokemon from './Lista Pokemon.png';
+import listaLigas from './Lista Ligas.png';
+import listaTipos from './Lista Tipos.png';
+import datosPokemon from './Datos Pokemon.png';
+import datosTipo from './Datos Tipo.png';
 
 function Pokeapi () {
     return (
-        <div className="Cuerpo">
+        <div className="CuerpoPokeapi">
             <h1 className="Titulo">
                 Pokeapi
             </h1>
@@ -13,6 +18,13 @@ function Pokeapi () {
                 Asimismo, en caso de desearlo, también puede optar por traer la lista de pokémons de una sola liga o un tipo, y así
                 agilizar su aplicación para evitar tiempos de carga exageradamente largos.
             </h2>
+            <div className="Muestra">
+                <img src={listaPokemon} alt="listaPokemon" className="Cartel" />
+                <img src={listaLigas} alt="listaLigas" className="Cartel" />
+                <img src={listaTipos} alt="listaTipos" className="Cartel" />
+                <img src={datosPokemon} alt="datosPokemon" className="Cartel" />
+                <img src={datosTipo} alt="datosTipo" className="Cartel" />
+            </div>
         </div>
     )
 }
